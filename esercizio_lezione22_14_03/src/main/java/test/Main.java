3e308bc04bd24f1894889f866e54ce138a3784de
Scoru <153868898+ItsScoru@users.noreply.github.com>
2024-03-15 10:56:42 +0100

    Implemented Main class to test PersonaDaoImpl and AutomobileDaoImpl

--- esercizio_lezione22_14_03/src/main/java/test/Main.java
+++ esercizio_lezione22_14_03/src/main/java/test/Main.java
@@ -1,10 +1,24 @@
 package test;
 
+import dao.AutomobileDaoImpl;
+import dao.InterfacciaDao;
+import dao.PersonaDaoImpl;
+import entity.Automobile;
+import entity.Persona;
+
 public class Main {
 
     public static void main(String[] args) {
         
-        //TO-DO generate examples
+        InterfacciaDao<Automobile> autoDao = new AutomobileDaoImpl();
+        
+        System.out.println(autoDao.findAll());
+        
+        System.out.println("\n°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°");
+        
+        InterfacciaDao<Persona> personaDao = new PersonaDaoImpl();
+        
+        System.out.println(personaDao.findAll());
 
     }
     
